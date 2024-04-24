11e61caeb6ddc65aa84fcb95fb9772223384acfa
Gargi2107 <100835773+Gargi2107@users.noreply.github.com>
2024-04-24 15:26:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+function showTab(tabName) {
+    // Hide all clothing sections
+    document.querySelectorAll('.clothing').forEach(function(el) {
+        el.style.display = 'none';
+    });
+    // Remove active class from all tabs
+    document.querySelectorAll('.tab').forEach(function(el) {
+        el.classList.remove('active');
+    });
+    // Show selected clothing section
+    document.getElementById(tabName).style.display = 'flex';
+    // Add active class to the clicked tab
+    document.querySelector(`.tab#${tabName}`).classList.add('active');
+}
